1bb56f8daac16d60d7d07a752e457f0e0b2aa8ea
Denis Zdorovtsov <mrtrizer@gmail.com>
2023-11-20 23:32:20 +0900

    Cleanup

--- Piper.cs
+++ Piper.cs
@@ -4,50 +4,50 @@ using System.Threading.Tasks;
 
 namespace Abuksigun.Piper
 {
-	public sealed unsafe class Piper : IDisposable
-	{
-		PiperLib.PiperConfig* config;
-		public PiperLib.PiperConfig* Config => config;
+    public sealed unsafe class Piper : IDisposable
+    {
+        PiperLib.PiperConfig* config;
+        public PiperLib.PiperConfig* Config => config;
 
-		Piper(PiperLib.PiperConfig* config)
-		{
+        Piper(PiperLib.PiperConfig* config)
+        {
             this.config = config;
         }
 
-		~Piper()
-		{
+        ~Piper()
+        {
             Dispose();
         }
 
-		public void Dispose()
-		{
-			if (config != null)
-			{
+        public void Dispose()
+        {
+            if (config != null)
+            {
                 PiperLib.terminatePiper(config);
                 PiperLib.destroy_PiperConfig(config);
                 config = null;
             }
-		}
+        }
 
-		public static Task<Piper> LoadPiper(string fullEspeakDataPath)
-		{
+        public static Task<Piper> LoadPiper(string fullEspeakDataPath)
+        {
             if (!Directory.Exists(fullEspeakDataPath))
                 throw new DirectoryNotFoundException("Espeak data directory not found");
 
             return Task.Run(() =>
-			{
-				var piperConfig = PiperLib.create_PiperConfig(fullEspeakDataPath);
-				try
-				{
-					PiperLib.initializePiper(piperConfig);
-					return Task.FromResult(new Piper(piperConfig));
-				}
-				catch
-				{
-					PiperLib.destroy_PiperConfig(piperConfig);
-					throw;
-				}
-			});
-		}
+            {
+                var piperConfig = PiperLib.create_PiperConfig(fullEspeakDataPath);
+                try
+                {
+                    PiperLib.initializePiper(piperConfig);
+                    return Task.FromResult(new Piper(piperConfig));
+                }
+                catch
+                {
+                    PiperLib.destroy_PiperConfig(piperConfig);
+                    throw;
+                }
+            });
+        }
     }
 }
